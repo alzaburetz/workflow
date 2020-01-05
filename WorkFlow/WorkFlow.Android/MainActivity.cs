1b1f250e891626153f7512caead3629f5d18c545
alzaburetz <anton.motin.dev@gmail.com>
2020-01-05 18:28:06 +0300

    Assembly settings changed, icon added

--- WorkFlow/WorkFlow.Android/MainActivity.cs
+++ WorkFlow/WorkFlow.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace WorkFlow.Droid
 {
-    [Activity(Label = "WorkFlow", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "WorkFlow", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
